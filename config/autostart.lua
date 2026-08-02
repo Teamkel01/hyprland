e8a9942cd74e54c29b127a00bc58d71d436315ff
Koshki <137661698+Teamkel01@users.noreply.github.com>
2026-08-02 20:26:33 +0000

    Add files via upload

---
+++ config/autostart.lua
@@ -0,0 +1,3 @@
+hl.on("hyprland.start", function ()
+    hl.exec_cmd("swaybg -i ~/Pictures/Wallpapers/dark-forest.jpg -m fill")
+end)
---
+++ config/windowrules.lua
@@ -0,0 +1,36 @@
+-- Kitty
+
+hl.window_rule({
+    name = "kitty",
+    match = {
+        class = "kitty"
+    },
+
+    float = true,
+    size = {1450, 850}
+})
+
+
+-- Librewolf
+
+hl.window_rule({
+    name = "librewolf",
+    match = {
+        class = "librewolf"
+    },
+
+    float = true,
+    size = {1450, 850}
+})
+
+-- Dolphin
+
+hl.window_rule({
+    name = "dolphin",
+    match = {
+        class = "org.kde.dolphin"
+    },
+
+    float = true,
+    size = {1450, 850}
+})
