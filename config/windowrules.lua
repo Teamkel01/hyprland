1886b0aa2c1221f180998e2c8b0f040c0f9b5193
Koshki <137661698+Teamkel01@users.noreply.github.com>
2026-08-03 12:53:18 +0000

    Add files via upload

--- config/windowrules.lua
+++ config/windowrules.lua
@@ -22,15 +22,3 @@ hl.window_rule({
     float = true,
     size = {1450, 850}
 })
-
--- Dolphin
-
-hl.window_rule({
-    name = "dolphin",
-    match = {
-        class = "org.kde.dolphin"
-    },
-
-    float = true,
-    size = {1450, 850}
-})
